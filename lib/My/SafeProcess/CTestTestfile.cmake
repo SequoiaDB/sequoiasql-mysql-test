771f665f4e3587c0f35b648205d9df82c437483c
root <191506569@qq.com>
2018-11-10 10:30:48 +0800

    remove some file after make

--- lib/My/SafeProcess/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ljh/mysql/mysql-5.7.18/mysql-test/lib/My/SafeProcess
-# Build directory: /home/ljh/mysql/mysql-5.7.18/mysql-test/lib/My/SafeProcess
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
